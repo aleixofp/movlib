76ff32eaa469342fc96365947ded3f2935c227a5
Felipe Aleixo <felipepaleixo@hotmail.com>
2017-09-17 11:31:51 -0300

    Add MovieDAO and its functions

--- src/com/fpa/softw/movlib/dao/MovieDAO.java
+++ src/com/fpa/softw/movlib/dao/MovieDAO.java
@@ -1,4 +1,64 @@
 package com.fpa.softw.movlib.dao;
 
+import com.fpa.softw.movlib.connection.ConnectionFactory;
+import com.fpa.softw.movlib.models.Movie;
+import com.fpa.softw.movlib.models.User;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieDAO {
+
+    private Connection connection;
+
+    public MovieDAO(){
+        this.connection = ConnectionFactory.getConnection();
+    }
+
+    public boolean addMovie(Movie movie){
+        return false;
+    }
+
+    public List<Movie> listMovieByUserId(int userId){
+        String sql = "SELECT * from movie WHERE user_id = " + userId;
+
+        List<Movie> movieList = new ArrayList<>();
+
+        try {
+            ResultSet rs = this.connection.prepareStatement(sql).executeQuery();
+
+            while (rs.next()){
+
+                int id = rs.getInt(1);
+                String title = rs.getString(2);
+                double userRating = rs.getDouble(3);
+                int watchedYear = rs.getInt(4);
+                boolean watched = rs.getBoolean(5);
+                boolean recommends = rs.getBoolean(6);
+
+
+                movieList.add(new Movie(id, title, userRating, watchedYear, watched, recommends, userId));
+
+            }
+
+            rs.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return movieList;
+    }
+
+    public void removeMovie(Movie movie){
+
+    }
+
+    public void updateMovie(int movieId){
+
+    }
+
 }
