4196a7ef49ed7c3318a4acfc04c9669f91cdd24b
Felipe Aleixo <felipepaleixo@hotmail.com>
2017-09-17 13:46:53 -0300

    Add buttons for Edit Movie and Remove Movie, and functionality for removing

--- src/com/fpa/softw/movlib/controllers/LobbyController.java
+++ src/com/fpa/softw/movlib/controllers/LobbyController.java
@@ -5,13 +5,17 @@ import com.fpa.softw.movlib.main.LoggedInUser;
 import com.fpa.softw.movlib.main.Main;
 import com.fpa.softw.movlib.models.Movie;
 import com.fpa.softw.movlib.models.User;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
 import java.util.List;
+import java.util.Optional;
 
 public class LobbyController {
 
@@ -23,33 +27,74 @@ public class LobbyController {
     @FXML
     private ListView lvMovies;
 
+    @FXML
+    private Button btnAddMovie, btnEditMovie, btnRemoveMovie;
+
+    private Movie selectedMovie;
+
+
+    private MovieDAO movieDAO;
 
     @FXML
     private void initialize(){
+
+        this.movieDAO = new MovieDAO();
+
         this.user = LoggedInUser.getLoggedInUser();
 
         this.lblName.setText(this.user.getName());
         this.lblUsername.setText(this.user.getUsername());
 
-        MovieDAO movieDAO = new MovieDAO();
-
-        List<Movie> userMovieList = movieDAO.listMovieByUserId(this.user.getId());
-
-        for (Movie mv:
-             userMovieList) {
-            System.out.println(mv.getTitle());
-        }
-
-        ObservableList<Movie> movieObservableList = FXCollections.observableArrayList();
-        movieObservableList.addAll(userMovieList);
+        //noinspection unchecked
+        this.lvMovies.setItems(this.getUpdatedMovieList());
 
         //noinspection unchecked
-        this.lvMovies.setItems(movieObservableList);
+        this.lvMovies.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if(btnEditMovie.isDisable() || btnRemoveMovie.isDisable()){
+                btnEditMovie.setDisable(false);
+                btnRemoveMovie.setDisable(false);
+
+            }
+
+
+            selectedMovie = (Movie)newValue;
+        });
     }
 
     @FXML
     private void addMovie(){
         Main.changeScene("add_movie.fxml");
+    }
+
+    @FXML
+    private void editMovie(){
+
+    }
+
+    @FXML
+    private void removeMovie(){
+        Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION,
+                "Are you sure you want to delete '" + this.selectedMovie.getTitle() + "'?", ButtonType.YES, ButtonType.NO);
+
+        Optional<ButtonType> choice = confirmationAlert.showAndWait();
+
+        if (choice.isPresent()){
+
+            if(choice.get().getButtonData().isDefaultButton()){
+                this.movieDAO.removeMovie(this.selectedMovie);
+                new Alert(Alert.AlertType.INFORMATION, "Movie removed.").showAndWait();
+
+                this.lvMovies.setItems(this.getUpdatedMovieList());
+            }
+
+        }
+
+    }
+
+    private ObservableList<Movie> getUpdatedMovieList(){
+        ObservableList<Movie> movieList = FXCollections.observableArrayList();
+        movieList.addAll(this.movieDAO.listMovieByUserId(this.user.getId()));
+        return movieList;
     }
 
 
