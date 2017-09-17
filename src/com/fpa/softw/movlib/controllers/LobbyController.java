c9cf2dfc087a6d78fab1a2de4f91250d793205f0
Felipe Aleixo <felipepaleixo@hotmail.com>
2017-09-17 11:31:06 -0300

    Add ListView and some layout changes

--- src/com/fpa/softw/movlib/controllers/LobbyController.java
+++ src/com/fpa/softw/movlib/controllers/LobbyController.java
@@ -2,8 +2,10 @@ package com.fpa.softw.movlib.controllers;
 
 import com.fpa.softw.movlib.main.LoggedInUser;
 import com.fpa.softw.movlib.models.User;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class LobbyController {
 
@@ -13,13 +15,17 @@ public class LobbyController {
     private Label lblName, lblUsername;
 
     @FXML
+    private ListView lvMovies;
+
+
+    @FXML
     private void initialize(){
         this.user = LoggedInUser.getLoggedInUser();
 
-        System.out.println(this.user.getUsername());
-
         this.lblName.setText(this.user.getName());
         this.lblUsername.setText(this.user.getUsername());
+
+
     }
 
 
