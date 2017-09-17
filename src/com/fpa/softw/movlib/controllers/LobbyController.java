fd4a4f9ce5505afcec4f308ef4b702e031abc5bd
Felipe Aleixo <felipepaleixo@hotmail.com>
2017-09-17 12:23:10 -0300

    Add functionality to populate list view and buttons on layout

--- src/com/fpa/softw/movlib/controllers/LobbyController.java
+++ src/com/fpa/softw/movlib/controllers/LobbyController.java
@@ -1,11 +1,17 @@
 package com.fpa.softw.movlib.controllers;
 
+import com.fpa.softw.movlib.dao.MovieDAO;
 import com.fpa.softw.movlib.main.LoggedInUser;
+import com.fpa.softw.movlib.main.Main;
+import com.fpa.softw.movlib.models.Movie;
 import com.fpa.softw.movlib.models.User;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+
+import java.util.List;
 
 public class LobbyController {
 
@@ -25,7 +31,25 @@ public class LobbyController {
         this.lblName.setText(this.user.getName());
         this.lblUsername.setText(this.user.getUsername());
 
+        MovieDAO movieDAO = new MovieDAO();
 
+        List<Movie> userMovieList = movieDAO.listMovieByUserId(this.user.getId());
+
+        for (Movie mv:
+             userMovieList) {
+            System.out.println(mv.getTitle());
+        }
+
+        ObservableList<Movie> movieObservableList = FXCollections.observableArrayList();
+        movieObservableList.addAll(userMovieList);
+
+        //noinspection unchecked
+        this.lvMovies.setItems(movieObservableList);
+    }
+
+    @FXML
+    private void addMovie(){
+        Main.changeScene("add_movie.fxml");
     }
 
 
